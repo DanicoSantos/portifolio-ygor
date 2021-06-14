a2970d4cfc59d5dd6ddc7e7f72953fac6d47e8b8
Daniel Santos <dados.dan.santos@gmail.com>
2021-06-14 14:58:57 -0300

    feat: create home page banner

---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+    <div class="columns is-centered is-desktop">
+      <div class="banner column has-text-centered">
+        <h1 class="title is-1">Visual designer</h1>
+        <p class="subtitle is-4 px-2">
+          Projetos de identidade visual, editorial e sites.
+        </p>
+        <a
+          href=""
+          class="button is-secondary is-large is-rounded is-size-4 has-text-weight-bold"
+          >Orçamento</a
+        >
+      </div>
+      <div
+        class="navigation column is-flex is-justify-content-center is-align-content-center"
+      >
+        <NavBar />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavBar from "../components/NavBar.vue";
+
+export default {
+  name: "Home",
+  components: {
+    NavBar,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../../node_modules/bulma";
+
+.is-secondary {
+  background-color: #a0a0a0;
+  width: 275px;
+  height: 75px;
+  box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
+}
+</style>
