200cdda446a97baeb0a426d2b5d02a44a3204291
Daniel Santos <dados.dan.santos@gmail.com>
2021-06-14 14:59:23 -0300

    fix: re-create client app

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="app">
+    <header class="app__header py-3 mb-3 is-flex is-justify-content-center is-align-items-center">
+      <div class="is-flex justify-content-center mt-6">
+        <img
+          src="https://via.placeholder.com/60"
+          width="60"
+          height="60"
+          
+        />
+      </div>
+    </header>
+    <section class="content section">
+      <router-view />
+    </section>
+  </div>
+</template>
+
+<style lang="scss">
+@import "../node_modules/bulma";
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+
+.app__header {
+  box-shadow: 5px 5px 8px rgba(0, 0, 0, 0.2);
+}
+
+</style>
