c2c456f531d235d7fe09965c584d8cb45bee36f0
Daniel Santos <dados.dan.santos@gmail.com>
2021-06-14 14:58:11 -0300

    feat; create navbar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="nav" class="is-uppercase">
+    <router-link to="/">Projetos</router-link> |
+    <router-link to="/about">Contato</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+}
+</script>
+
+<style lang="scss">
+@import '../../node_modules/bulma';
+</style>
+
