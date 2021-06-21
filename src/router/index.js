0f90d0acf9e626e8124cf167b97bd613611d54e6
Daniel Santos <dados.dan.santos@gmail.com>
2021-06-21 13:56:54 -0300

    feat: create brand view

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,38 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Brands from '../views/Brands.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Brands from "../views/Brands.vue";
+import Brand from "../views/Brand.vue";
+
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/contato',
-    name: 'Contact',
+    path: "/contato",
+    name: "Contact",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Contact.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Contact.vue"),
   },
   {
-    path: '/marcas',
-    name: 'Brands',
-    component: Brands
-  }
-]
+    path: "/marcas",
+    name: "Brands",
+    component: Brands,
+  },
+  {
+    path: "/marca",
+    name: "Brand",
+    component: Brand,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/Brand.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="brand">
+    <h1>Marca X</h1>
+  </div>
+</template>
+
+<style lang="scss">
+@import "../../node_modules/bulma";
+</style>
